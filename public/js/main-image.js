867e76dbf6ea08805dbde015e9978b30b82238d7
fukutitakahide <ft0115bd@gmail.com>
2022-06-11 00:28:01 +0000

    アプリケーション追加

--- public/js/main-image.js
+++ public/js/main-image.js
@@ -1,41 +1,84 @@
 function showimg1(){
-     document.getElementById("area1").setAttribute("src","../img/fast/login.jpg");
+     document.getElementById("area1").setAttribute("src","public/img/fast/login.jpg");
 }
 function showimg2(){
-     document.getElementById("area1").setAttribute("src","../img/fast/news-page.jpg");
+     document.getElementById("area1").setAttribute("src","public/img/fast/news-page.jpg");
 }
 function showimg3(){
-     document.getElementById("area1").setAttribute("src","../img/fast/top-touroku.jpg");
+     document.getElementById("area1").setAttribute("src","public/img/fast/top-touroku.jpg");
 }
 function showimg4(){
-     document.getElementById("area1").setAttribute("src","../img/fast/top.jpg");
+     document.getElementById("area1").setAttribute("src","public/img/fast/top.jpg");
 }
 function showimg5(){
-     document.getElementById("area1").setAttribute("src","../img/fast/touroku.jpg");
+     document.getElementById("area1").setAttribute("src","public/img/fast/touroku.jpg");
 }
 function showimg6(){
-     document.getElementById("area1").setAttribute("src","../img/fast/user-edit.jpg");
+     document.getElementById("area1").setAttribute("src","public/img/fast/user-edit.jpg");
 }
 function showimg7(){
-     document.getElementById("area2").setAttribute("src","../img/qanda/answer.jpg");
+     document.getElementById("area2").setAttribute("src","public/img/qanda/answer.jpg");
 }
 function showimg8(){
-     document.getElementById("area2").setAttribute("src","../img/qanda/new.jpg");
+     document.getElementById("area2").setAttribute("src","public/img/qanda/new.jpg");
 }
 function showimg9(){
-     document.getElementById("area2").setAttribute("src","../img/qanda/question.jpg");
+     document.getElementById("area2").setAttribute("src","public/img/qanda/question.jpg");
 }
+// 画像3始まり
 function showimg11(){
-     document.getElementById("area3").setAttribute("src","../img/fukulog/vlog-about-me.png");
+     document.getElementById("area3").setAttribute("src","public/img/fukulog/vlog-about-me.png");
 }
 function showimg12(){
-     document.getElementById("area3").setAttribute("src","../img/fukulog/vlog-contact.png");
+     document.getElementById("area3").setAttribute("src","public/img/fukulog/vlog-contact.png");
 }
 function showimg13(){
-     document.getElementById("area3").setAttribute("src","../img/fukulog/vlog-top.png");
+     document.getElementById("area3").setAttribute("src","public/img/fukulog/vlog-top.png");
 }
 function showimg14(){
-     document.getElementById("area3").setAttribute("src","../img/fukulog/vlog-profile.png");
+     document.getElementById("area3").setAttribute("src","public/img/fukulog/vlog-profile.png");
+}
+// 画像4始まり
+function showimg15(){
+     document.getElementById("area4").setAttribute("src","public/img/photo-search-app/photo-search-app-1.png");
+}
+function showimg16(){
+     document.getElementById("area4").setAttribute("src","public/img/photo-search-app/photo-search-app-2.png");
+}
+// 画像5始まり
+function showimg17(){
+     document.getElementById("area5").setAttribute("src","public/img/recipelog/login.png");
+}
+function showimg18(){
+     document.getElementById("area5").setAttribute("src","public/img/recipelog/recipe-all.png");
+}
+function showimg19(){
+     document.getElementById("area5").setAttribute("src","public/img/recipelog/recipe-edit.png");
+}
+function showimg20(){
+     document.getElementById("area5").setAttribute("src","public/img/recipelog/recipe-show.png");
+}
+function showimg21(){
+     document.getElementById("area5").setAttribute("src","public/img/recipelog/recipelog-top.png");
+}
+function showimg22(){
+     document.getElementById("area5").setAttribute("src","public/img/recipelog/signup.png");
+}
+function showimg23(){
+     document.getElementById("area5").setAttribute("src","public/img/recipelog/user-all.png");
+}
+function showimg24(){
+     document.getElementById("area5").setAttribute("src","public/img/recipelog/user-edit.png");
+}
+function showimg25(){
+     document.getElementById("area5").setAttribute("src","public/img/recipelog/user-show.png");
+}
+// 画像6始まり
+function showimg26(){
+     document.getElementById("area6").setAttribute("src","public/img/todoapp/todoapp-1.png");
+}
+function showimg27(){
+     document.getElementById("area6").setAttribute("src","public/img/todoapp/todoapp-2.png");
 }
 (function() {
     ('.hamburger').click(function() {
